265a576a8ed0e35c9f7c51a1d5352291d5facef5
RanMarkovich <markovich.ran@gmail.com>
2025-09-24 22:18:23 +0300

    add s3 for remote state control

--- terraform/accounts/681048796370/s3/main.tf
+++ terraform/accounts/681048796370/s3/main.tf
@@ -1,6 +1,7 @@
-resource "aws_s3_bucket" "tf_state" {
+resource "aws_s3_bucket" "this" {
   for_each = var.state_file_path
   bucket   = each.value
+  force_destroy = true
 
   tags = {
     Name        = "terraform-remote-state"
--- terraform/accounts/681048796370/s3/variables.tf
+++ terraform/accounts/681048796370/s3/variables.tf
@@ -1,7 +1,24 @@
 variable "state_file_path" {
+  description = "The path to the state file"
   type = map(string)
-  default = {
-    s3  = "repos/iaclabs/${var.account_id}/s3/tf-state"
-    vpc = "repos/iaclabs/${var.account_id}/vpc/tf-state"
-  }
+}
+
+variable "account_id" {
+  description = "The account ID to deploy the S3"
+  type = string
+}
+
+variable "region" {
+  description = "The region to deploy the S3"
+  type = string
+}
+
+variable "access_key" {
+  description = "The access key to deploy the S3"
+  type = string
+}
+
+variable "secret_key" {
+  description = "The secret key to deploy the S3"
+  type = string
 }
